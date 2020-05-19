3292a7be242f011d53cd9aba62d8a18a88bbcff8
D1STR4CT <slmkuitert@gmail.com>
2020-05-19 12:35:35 +0200

    Fixed a few mistakes I made

--- passwordmanager_beta_2.0/passwordmanager.py
+++ passwordmanager_beta_2.0/passwordmanager.py
@@ -12,17 +12,17 @@ import base64
 
 
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def get_master_password_from_user():
     while True:
-        pass1 = getpass('Please enter your password: ')
-        pass2 = getpass('Please confirm your password: ')
+        pass1 = getpass("Please enter your password: ")
+        pass2 = getpass("Please confirm your password: ")
         if pass1 == pass2:
             return pass1
         else:
-            print('The passwords didn\'t match, try again please. ')
+            print("The passwords didn\"t match, try again please. ")
 
 
 def create_salt():
@@ -30,7 +30,7 @@ def create_salt():
 
 
 def get_user_salt():
-    f = open('salt.txt', 'rb')
+    f = open("salt.txt", "rb")
     salt = f.read()
     f.close()
     return salt
@@ -43,20 +43,20 @@ def create_encryption_key(password: str, salt: str):
 
 
 def encrypt_database_file(key: str):
-    password_database_file = open('pwddatabase.txt', 'rb')
+    password_database_file = open("pwddatabase.txt", "rb")
     password_database = password_database_file.read()
     password_database_file.close()
 
     fernet = Fernet(key)
     encrypted_password_database = fernet.encrypt(password_database)
 
-    password_database_file = open('pwddatabase.txt', 'wb')
+    password_database_file = open("pwddatabase.txt", "wb")
     password_database_file.write(encrypted_password_database)
     password_database_file.close()
 
 
 def get_decrypted_database(key: str):
-    password_database_file = open('pwddatabase.txt', 'rb')
+    password_database_file = open("pwddatabase.txt", "rb")
     password_database = password_database_file.read()
     password_database_file.close()
 
@@ -67,32 +67,32 @@ def get_decrypted_database(key: str):
 
 
 def save_database_encrypted(database_content: str, key: str):
-    password_database_file = open('pwddatabase.txt', 'wb')
+    password_database_file = open("pwddatabase.txt", "wb")
     password_database_file.write(database_content.encode())
     password_database_file.close()
     encrypt_database_file(key)
 
 
 def setup():
-    print('Starting the setup...')
-    print('Enter the password you would like to use. ')
-    print('This password will be used to login and encrypt/decrypt the database. ')
+    print("Starting the setup...")
+    print("Enter the password you would like to use. ")
+    print("This password will be used to login and encrypt/decrypt the database. ")
     global master_password
     master_password = get_master_password_from_user()
-    salt_file = open('salt.txt', 'wb')
+    salt_file = open("salt.txt", "wb")
     salt_file.write(create_salt())
     salt_file.close()
-    password_database_file = open('pwddatabase.txt', 'w')
+    password_database_file = open("pwddatabase.txt", "w")
     password_database_file.write("Passwords will be saved in the format: \n" + "Username : Passwords \n")
     password_database_file.close()
     encrypt_database_file(create_encryption_key(master_password, get_user_salt()))
-    input('Setup complete \nPress enter to continue')
+    input("Setup complete \nPress enter to continue")
     clear_screen() 
 
 
 def authenticate_user(failed = False):
     if failed:
-        print('The entered password is incorrect, try again please')
+        print("The entered password is incorrect, try again please")
 
     provided_password = getpass("Please enter your masterpassword: ")
     database_decrypted = None
@@ -112,11 +112,11 @@ def save_password():
         password = getpass("Enter password: ")
         password_check = getpass("Please confirm password")
         if password == password_check:
-            database += f'{username} : {pasword}\n'
+            database += f"{username} : {pasword}\n"
             save_database_encrypted(database, key)
             break
         else: 
-            print("Passwords dit not match, please try again.")
+            print("Passwords did not match, please try again.")
 
 def view_passwords():
     database = get_decrypted_database(create_encryption_key(master_password, get_user_salt()))
@@ -126,20 +126,21 @@ def view_passwords():
 
 
 def change_master_password():
-    new_master_password = getpass('Please enter your new password: ')
+    new_master_password = getpass("Please enter your new password: ")
+    new_master_password_check = getpass("Please confirm your password: ")
     database = get_decrypted_database(create_encryption_key(master_password, get_user_salt()))
     new_salt = create_salt()
-    salt_file = open('salt.txt', 'wb')
+    salt_file = open("salt.txt", "wb")
     salt_file.write(new_salt)
     salt_file.close()
     save_database_encrypted(database, create_encryption_key(new_master_password, new_salt))
-    print('The password has been changed!')
+    print("The password has been changed!")
     time.sleep(1)
 
 
 def main():
     clear_screen()
-    if os.path.exists('pwddatabase.txt') == False or os.path.exists('salt.txt') == False:
+    if os.path.exists("pwddatabase.txt") == False or os.path.exists("salt.txt") == False:
         # At least one of the important files is missing so start the setup mode
         setup()
 
@@ -154,21 +155,21 @@ def main():
         print("[3] Change masterpassword")
         print("[x] Exit")
         menu_choice = input("Choice: ")
-        if menu_choice == '1':
+        if menu_choice == "1":
             clear_screen()
             view_passwords()
             clear_screen()
-        elif menu_choice == '2':
+        elif menu_choice == "2":
             clear_screen()
             save_password()
             clear_screen()
-        elif menu_choice == '3':
+        elif menu_choice == "3":
             clear_screen()
             change_master_password()
             clear_screen()
-        elif menu_choice == 'x':
+        elif menu_choice == "x":
             exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
