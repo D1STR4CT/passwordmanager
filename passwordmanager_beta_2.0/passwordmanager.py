db11bb5bb359778bf8b9014c19d1bc8b83423ff9
D1STR4CT <slmkuitert@gmail.com>
2020-05-19 12:43:43 +0200

    Fixed another few typos that anoyed me

--- passwordmanager_beta_2.0/passwordmanager.py
+++ passwordmanager_beta_2.0/passwordmanager.py
@@ -83,7 +83,7 @@ def setup():
     salt_file.write(create_salt())
     salt_file.close()
     password_database_file = open("pwddatabase.txt", "w")
-    password_database_file.write("Passwords will be saved in the format: \n" + "Username : Passwords \n")
+    password_database_file.write("MyPasswordVault: \n" + "Username : Passwords \n")
     password_database_file.close()
     encrypt_database_file(create_encryption_key(master_password, get_user_salt()))
     input("Setup complete \nPress enter to continue")
@@ -110,7 +110,7 @@ def save_password():
     username = input("Input username: ")
     while True: 
         password = getpass("Enter password: ")
-        password_check = getpass("Please confirm password")
+        password_check = getpass("Please confirm password: ")
         if password == password_check:
             database += f"{username} : {password}\n"
             save_database_encrypted(database, key)
@@ -121,7 +121,7 @@ def save_password():
 def view_passwords():
     database = get_decrypted_database(create_encryption_key(master_password, get_user_salt()))
     print(database)
-    print("Press a key to continue...")
+    print("Press enter key to continue...")
     input()
 
 
